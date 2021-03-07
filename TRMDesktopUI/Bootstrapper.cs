8498d4aa782abba2c913ce9e015cbb6f9c90d9e0
Rodger <Rodger@MEDIA>
2021-03-07 16:46:43 -0500

    Update from Tim

---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            AppDomainInitializer();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewfor<ShellViewModel>();
+        }
+    }
+}
